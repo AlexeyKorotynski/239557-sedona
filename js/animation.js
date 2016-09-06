127d1bf10ca04392f38a7bb06a05a626bd924e0c
AlexeyKorotynski <lexa1997k@gmail.com>
2016-09-07 00:17:21 +0300

    доработка проекта

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,18 @@
-function initialize(){var a={zoom:9,center:new google.maps.LatLng(34.8018134,-111.6061398)};new google.maps.Map(document.querySelector(".map"),a)}var link=document.querySelector(".search-form__link"),popup=document.querySelector(".input"),input=document.querySelector(".input--show");link.addEventListener("click",function(a){a.preventDefault(),popup.classList.contains("input--show")?popup.classList.remove("input--show"):popup.classList.add("input--show")}),google.maps.event.addDomListener(window,"load",initialize);
+var link = document.querySelector(".search-form__link");
+var popup = document.querySelector(".input");
+var input = document.querySelector(".input--show")
+link.addEventListener("click", function(event){
+  event.preventDefault();
+if(!popup.classList.contains("input--show"))
+  popup.classList.add("input--show");
+else
+  popup.classList.remove("input--show")
+});
+function initialize() {
+  var mapOptions = {
+    zoom: 9,
+    center: new google.maps.LatLng(34.8018134,-111.6061398)
+  }
+  var map = new google.maps.Map(document.querySelector('.map'), mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
